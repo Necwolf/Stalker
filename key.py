8c7434f3dc152adf2bb2fc306ed5fcdc1c521efd
Necwolf <40932739+Necwolf@users.noreply.github.com>
2019-11-07 21:04:31 +0200

    Update key.py

--- key.py
+++ key.py
@@ -4,5 +4,10 @@ from telebot.types import *
 
 
 key = ReplyKeyboardMarkup(resize_keyboard=True)
-key.row ('📁Профиль','кнопка которая не рабоате')
-key.row('тоже тест')
+key.row ('📁Профиль')
+
+
+
+KNB = InlineKeyboardMarkup( )
+KNB.add(InlineKeyboardButton('Камень', callback_data= 'Камень'),InlineKeyboardButton('ножницы', callback_data= 'ножницы'),InlineKeyboardButton('бумага', callback_data= 'бумага'),InlineKeyboardButton('ящерица', callback_data= 'ящерица'),InlineKeyboardButton('спок', callback_data= 'спок'))
+KNB.add(InlineKeyboardButton('Статистика игр', callback_data= 'cb_stat'),InlineKeyboardButton('Выход', callback_data= 'cb_exit'))
