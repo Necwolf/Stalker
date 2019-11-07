0713a010981810abb121b873cde5a2532128872e
Necwolf <40932739+Necwolf@users.noreply.github.com>
2019-11-07 21:06:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,16 @@
 
 
-TOKEN = '969215766:AAEP-TPcx3v8Ct1sMR-4dmC_hNgCoWhR0kI'
+TOKEN = '<YOU_TOKEN>'
+
+operators =[188539449,657633129]
+
+#Чаты Банд
+
+tryasi = -1001338670018 # Трячи Шатай!
+wolf = -1001483645531 # Волчий отряд
+suiside = -1001434598079 # Отряд самоубитц
+
+#Читаймые каналы
+
+channel_read = [-1001360694345, -1001470701250, -1001171584711]
+group_frwd_channel = [tryasi, wolf, suiside]
