cfa978b77301e2a34705b08617e8831bf1415a71
Necwolf <40932739+Necwolf@users.noreply.github.com>
2020-03-04 22:22:25 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,27 @@
+import os
 
+TOKEN = '<TOKEN>'
+#данные для базы
+dblogin = '<LOGIN>'
+dbpsw = '<PASS>'
+dbhost = '<HOST>'
+db = "<DBNAME>"
+#KEY_DATABASE = os.getenv("DATABASE_URL")
+DATABASE = f'postgresql+psycopg2://{dblogin}:{dbpsw}@{dbhost}:5432/{db}'
 
-TOKEN = '<YOU_TOKEN>'
-
+#Операторы
 operators =[188539449,657633129]
 
-#Чаты Банд
+ #Чаты Банд
 
-tryasi = -1001338670018 # Трячи Шатай!
-wolf = -1001483645531 # Волчий отряд
-suiside = -1001434598079 # Отряд самоубитц
+tryasi = -1001338670018  # Трячи Шатай!
+wolf = -1001483645531  # Волчий отряд
+suicide = -1001434598079  # Отряд самоубитц
 
-#Читаймые каналы
+ #Читаймые каналы
 
 channel_read = [-1001360694345, -1001470701250, -1001171584711]
-group_frwd_channel = [tryasi, wolf, suiside]
+group_frwd_channel = [tryasi, wolf, suicide]
+
+
+# URL webhookset
